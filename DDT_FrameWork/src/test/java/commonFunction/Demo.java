b7b24670891f6dfdf0cdba4ed61ecf1bc009db5b
Ramakrishna Poola <ramakrishna.poola25@gmail.com>
2025-04-09 13:21:31 +0530

    New Class Created

---
+++ DDT_FrameWork/src/test/java/commonFunction/Demo.java
@@ -0,0 +1,16 @@
+package commonFunction;
+
+public class Demo {
+	public void add()
+	{
+		System.out.println("add");
+	}
+	public void div()
+	{
+		System.out.println("div");
+	}
+	public void mul()
+	{
+		System.out.println("mul");
+	}
+}
